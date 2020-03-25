005fcd1f4debf7676e312634b9c1f4b60427eef2
mohammadrezanajari <mr.najari@gmail.com>
2020-03-25 22:22:46 +0430

    first commit

---
+++ 01-variable/sample-types.js
@@ -0,0 +1,6 @@
+var found = true;
+var firstName = "reza";
+var grade = 6.6;
+var lastName = 'najari';
+found = false;
+console.log("hi " + found);
---
+++ 01-variable/sample-types.ts
@@ -0,0 +1,10 @@
+
+let found:boolean=true;
+let firstName:string="reza";
+let grade:number=6.6;
+let lastName:string='najari';
+found =false;
+
+
+
+console.log(`hi ${found}`);
